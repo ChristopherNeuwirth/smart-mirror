2f4ea32b6c8dfcd07cd7ec580a74ace5f59dc3a2
Christopher Neuwirth <christopher.neuwirth@mhp.com>
2019-12-08 17:58:10 +0100

    chore: project renaming

--- README.md
+++ README.md
@@ -3,15 +3,27 @@
 ## Konstruktion
 
 Frameless
+
 Kabel dahinter verschwinden
+
 Rapberry leicht zugänglich
 
 ## Features
 
 Abfahrtszeiten
+
 Wetter
+
 News
+
 Guten Morgen
+
 Auto off and on
+
 Musik abspielen
+
 Sprach Bedienung
+
+## Deployment
+
+[lighttpd](https://www.elektronik-kompendium.de/sites/raspberry-pi/1905271.htm) benutzen
--- ui/src/app/app.component.spec.ts
+++ ui/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'starter-ui'`, () => {
+  it(`should have as title 'smart-mirror'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('starter-ui');
+    expect(app.title).toEqual('smart-mirror');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to starter-ui!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to smart-mirror!');
   });
 });
